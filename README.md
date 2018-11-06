d29b57a6a3338b6d8d1080b84d147470706d25fd
Administrator <ryo.elevensoul@gmail.com>
2018-11-06 22:30:28 +0900

    第01回 読書会 3章 rspecの構文まで

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EveryDayRailsBookClub
+
 書籍「Everyday Rails - RSpecによるRailsテスト入門」の読書会用リポジトリ
+
+## 開催
+
+* 第01回 2018/11/06(火)
---
+++ studying_everydayrails-rspec-2017/spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # 姓、名、メール、パスワードがあれば有効な状態であること
+  it "is valid with a first name, last name, email, and password" do
+    user = User.new(
+      first_name: "Aaron",
+      last_name: "Sumner",
+      email: "tester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze"
+    )
+    expect(user).to be_valid
+  end
+
+  # 名がなければ無効な状態であること
+  it "is invalid without a first name"
+  # 姓がなければ無効な状態であること
+  it "is invalid without a last name"
+  # メールアドレスがなければ無効な状態であること
+  it "is invalid without an email address"
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address"
+  # ユーザーのフルネームを文字列として返すこと
+  it "returns a user's full name as a string"
+end
