a4fa711fc4dd85b2bc1d62903efef9535fc55481
Administrator <ryo.elevensoul@gmail.com>
2018-11-15 22:32:33 +0900

    第02回読書会 3章 インスタンスメソッドをテストする

--- studying_everydayrails-rspec-2017/spec/models/user_spec.rb
+++ studying_everydayrails-rspec-2017/spec/models/user_spec.rb
@@ -13,13 +13,50 @@ RSpec.describe User, type: :model do
   end
 
   # 名がなければ無効な状態であること
-  it "is invalid without a first name"
+  it "is invalid without a first name" do
+    user = User.new(
+      first_name: nil
+    )
+    user.valid?
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
   # 姓がなければ無効な状態であること
-  it "is invalid without a last name"
+  it "is invalid without a last name" do
+    user = User.new(last_name: nil)
+    user.valid?
+    expect(user.errors[:last_name]).to include("can't be blank") 
+  end
+
   # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address"
   # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address"
+  it "is invalid with a duplicate email address" do
+    User.create(
+      first_name: "Ryo",
+      last_name: "imahashi",
+      email: "r.imahashixxxx@email.com",
+      password: "P@ssW0rd"
+    )
+
+    user = User.new(
+      first_name: "Jane",
+      last_name: "imahashi",
+      email: "r.imahashixxxx@email.com",
+      password: "P@ssWord"
+    )
+
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+
+  end
   # ユーザーのフルネームを文字列として返すこと
-  it "returns a user's full name as a string"
+  it "returns a user's full name as a string" do
+    user = User.new(
+      first_name: "Ryo",
+      last_name: "Tsumura",
+      email: "test@example.com"
+    )
+    expect(user.name).to eq "Ryo Tsumura"
+  end
 end
